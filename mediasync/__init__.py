ad1093e6974f117eaf416ac3b330d8dd8f086fcb
Greg Taylor <gtaylor@duointeractive.com>
2010-11-12 14:37:01 -0500

    Added the EMULATE_COMBO setting. When SERVE_REMOTE == False and EMULATE_COMBO == True, combo files are served locally, instead of being broken up into their component files.

--- mediasync/__init__.py
+++ mediasync/__init__.py
@@ -1,5 +1,7 @@
-import mimetypes
 import os
+import cStringIO
+import mimetypes
+from django.conf import settings
 
 JS_MIMETYPES = (
     "application/javascript",
@@ -32,7 +34,7 @@ def listdir_recursive(dir_str):
         for dir_candidate in dirs:
             if not is_syncable_dir(dir_candidate):
                 dirs.remove(dir_candidate)
-  
+
         basename = os.path.basename(root)
         if is_syncable_dir(basename):
             for file in files:
@@ -41,7 +43,43 @@ def listdir_recursive(dir_str):
                     fname = fname[1:]
                 yield fname
         else:
-             pass # "Skipping directory %s" % root
+            # "Skipping directory %s" % root
+            pass
+
+def combine_files(joinfile, sourcefiles, client):
+    """
+    Given a combo file name (joinfile), combine the sourcefiles into a single
+    monolithic file.
+    
+    Returns a string containing the combo file, or None if the specified
+    file can not be combo'd.
+    """
+    CSS_PATH = settings.MEDIASYNC.get("CSS_PATH", "").strip('/')
+    JS_PATH = settings.MEDIASYNC.get("JS_PATH", "").strip('/')
+
+    joinfile = joinfile.strip('/')
+
+    if joinfile.endswith('.css'):
+        dirname = CSS_PATH
+    elif joinfile.endswith('.js'):
+        dirname = JS_PATH
+    else:
+        # By-pass this file since we only join CSS and JS.
+        return None
+
+    buffer = cStringIO.StringIO()
+
+    for sourcefile in sourcefiles:
+        sourcepath = os.path.join(client.media_root, dirname, sourcefile)
+        if os.path.isfile(sourcepath):
+            f = open(sourcepath)
+            buffer.write(f.read())
+            f.close()
+            buffer.write('\n')
+
+    filedata = buffer.getvalue()
+    buffer.close()
+    return filedata
 
 def sync(client=None, force=False):
     """ Let's face it... pushing this stuff to S3 is messy.
@@ -49,91 +87,67 @@ def sync(client=None, force=False):
         and they have to be in a certain order as some variables rely
         on others.
     """
-    
-    from django.conf import settings
+
     from mediasync import backends
-    import cStringIO
-    
+
     assert hasattr(settings, "MEDIASYNC")
-    
-    CSS_PATH = settings.MEDIASYNC.get("CSS_PATH", "").strip('/')
-    JS_PATH = settings.MEDIASYNC.get("JS_PATH", "").strip('/')
-    
+
     # create client connection
     if client is None:
         client = backends.client()
-        
+
     client.open()
     client.serve_remote = True
 
     #
     # sync joined media
     #
-    
+
     joined = settings.MEDIASYNC.get("JOINED", {})
-    
+
     for joinfile, sourcefiles in joined.iteritems():
-        
-        joinfile = joinfile.strip('/')
-        
-        if joinfile.endswith('.css'):
-            dirname = CSS_PATH
-        elif joinfile.endswith('.js'):
-            dirname = JS_PATH
-        else:
-            continue # bypass this file since we only join css and js
-        
-        buffer = cStringIO.StringIO()
-        
-        for sourcefile in sourcefiles:
-            
-            sourcepath = os.path.join(client.media_root, dirname, sourcefile)
-            if os.path.isfile(sourcepath):
-                f = open(sourcepath)
-                buffer.write(f.read())
-                f.close()
-                buffer.write('\n')        
-        
-        filedata = buffer.getvalue()
-        buffer.close()
-        
+        filedata = combine_files(joinfile, sourcefiles, client)
+        if filedata is None:
+            # combine_files() is only interested in CSS/JS files.
+            continue
+
         content_type = mimetypes.guess_type(joinfile)[0] or 'application/octet-stream'
-        
+
         remote_path = joinfile
         if dirname:
             remote_path = "%s/%s" % (dirname, remote_path)
-        
+
         if client.process_and_put(filedata, content_type, remote_path, force=force):
             print "[%s] %s" % (content_type, remote_path)
-    
+
     #
     # sync static media
     #
 
     for dirname in os.listdir(client.media_root):
-        
+
         dirpath = os.path.abspath(os.path.join(client.media_root, dirname))
-        
+
         if os.path.isdir(dirpath):
-           
+
             for filename in listdir_recursive(dirpath):
-                
+
                 # calculate local and remote paths
                 filepath = os.path.join(dirpath, filename)
                 remote_path = "%s/%s" % (dirname, filename)
-                
+
                 content_type = mimetypes.guess_type(filepath)[0] or 'application/octet-stream'
-                
+
                 if not is_syncable_file(os.path.basename(filename)) or not os.path.isfile(filepath):
                     continue # hidden file or directory, do not upload
-                
+
                 filedata = open(filepath, 'rb').read()
-                
+
                 if client.process_and_put(filedata, content_type, remote_path, force=force):
                     print "[%s] %s" % (content_type, remote_path)
-    
+
     client.close()
 
 
-__all__ = ['sync','SyncException']
+__all__ = ['sync', 'SyncException']
 __version__ = '2.0.0dev'
